9db0f51359bc369dc5b59720cd0ca25e57b7756b
Lukas <lukas.kesch@gmail.com>
2020-02-07 00:10:44 +0100

    Design

---
+++ BwInf38Runde2Aufgabe2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BwInf38Runde2Aufgabe2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int Number;
+        int Digit;
+        int AbortCondition;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            Prepare();
+            AddOperator addOperator = new AddOperator(new AddOperator(2, 2).GetResult(), 4);
+            //MessageBox.Show(addOperator.GetResult().ToString());
+            PowerOperator powerOperator = new PowerOperator(5, 2);
+            MessageBox.Show(powerOperator.GetResult().ToString());
+        }
+        private void Prepare()
+        {
+            try
+            {
+                Number = int.Parse(TextBoxNumberToCalculate.Text);
+                Digit = int.Parse(TextBoxDigit.Text);
+                AbortCondition = DataProcessing.GetAbortCondition(Number, Digit);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Die eingegebenen Parameter konnten nicht entgegen genommen werden");
+                throw;
+            }
+        }
+        private void Recursion()
+        {
+
+        }
+    }
+}
---
+++ BwInf38Runde2Aufgabe2/Operators.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwInf38Runde2Aufgabe2
+{
+    public abstract class SingleInputOperator
+    {
+        protected double Number;
+
+        public SingleInputOperator(double Input)
+        {
+            Number = Input;
+        }
+        public virtual double GetResult()
+        {
+            return -1;
+        }
+    }
+    public class FactorialOperator : SingleInputOperator
+    {
+        public FactorialOperator(double Input) : base(Input) { }
+
+        public override double GetResult()
+        {
+            double result = 1;
+            while (Number != 1)
+            {
+                result *= Number;
+                Number--;
+            }
+            return base.GetResult();
+        }
+    }
+
+    public abstract class DoubleInputOperator
+    {
+        protected double Number1;
+        protected double Number2;
+        //Konstruktor
+        public DoubleInputOperator(double Input1, double Input2)
+        {
+            Number1 = Input1;
+            Number2 = Input2;
+        }
+        public virtual double GetResult()
+        {
+            return -1;
+        }
+    }
+
+    public class AddOperator : DoubleInputOperator
+    {
+        public AddOperator(double Input1, double Input2) : base(Input1, Input2) { }
+
+        public override double GetResult()
+        {
+            return Number1 + Number2;
+        }
+    }
+
+    public class SubtractOperator : DoubleInputOperator
+    {
+        public SubtractOperator(double Input1, double Input2) : base(Input1, Input2) { }
+
+        public override double GetResult()
+        {
+            return Number1 - Number2;
+        }
+    }
+
+    public class MultiplyOperator : DoubleInputOperator
+    {
+        public MultiplyOperator(double Input1, double Input2) : base(Input1, Input2) { }
+
+        public override double GetResult()
+        {
+            return Number1 * Number2;
+        }
+    }
+
+    public class DivisionOperator : DoubleInputOperator
+    {
+        public DivisionOperator(double Input1, double Input2) : base(Input1, Input2) { }
+
+        public override double GetResult()
+        {
+            return Number1 / Number2;
+        }
+    }
+
+    public class PowerOperator : DoubleInputOperator
+    {
+        public PowerOperator(double Input1, double Input2) : base(Input1, Input2) { }
+
+        public override double GetResult()
+        {
+            return Math.Pow(Number1, Number2);
+        }
+    }
+}
