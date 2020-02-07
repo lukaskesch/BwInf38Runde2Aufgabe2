bb0bb198beeba2a88639ea3d2beef668c91e939f
Lukas <lukas.kesch@gmail.com>
2020-02-07 16:08:39 +0100

    Implementation of CreateTerms (not finished)

--- BwInf38Runde2Aufgabe2/Calculation.cs
+++ BwInf38Runde2Aufgabe2/Calculation.cs
@@ -51,49 +51,83 @@ namespace BwInf38Runde2Aufgabe2
                 //ListTerms[nDigit].Add(NewLiteral); muss noch überprüft werden, ob wert nicht schon erreicht
 
                 //Gehe jede Ziffernlänge bis eins vor das Aktuelle durch
-                for (int i = 0; i < nDigit; i++)
+                for (int DigitLenght = 0; DigitLenght < nDigit; DigitLenght++)
                 {
                     //Für jede Ziffernlänge gehe alle ihre Terme durch
-                    for (int j = 0; j < ListTerms[i].Count; j++)
+                    for (int ElementsOfDigitLength = 0; ElementsOfDigitLength < ListTerms[DigitLenght].Count; ElementsOfDigitLength++)
                     {
                         //Für jede dieser Terme verknüpfe sie mit mit nDigit-i
-                        int max = nDigit - 1;
-                        max = ListTerms[max].Count;
-                        for (int k = 0; k < 0; k++)
+                        int RemainingDigitDifference = nDigit - 1;
+                        RemainingDigitDifference = ListTerms[RemainingDigitDifference].Count;
+                        for (int ElementsOfRemainingDigitDifference = 0; ElementsOfRemainingDigitDifference < RemainingDigitDifference; ElementsOfRemainingDigitDifference++)
                         {
-                            //Erstelle Term
+                            //Erstelle sinnvolle Terme
                             //Überprüfe Term
+                            CreateTerms(DigitLenght,ElementsOfDigitLength, RemainingDigitDifference, ElementsOfRemainingDigitDifference);
                         }
                     }
                 }
-
-                //Überprüfe, ob das Ergebnis schon einmal erstellt wurde und ob es die erwünschte Zahl ist
-                if (false)
-                {
-
-                }
-                if (false)
-                {
-
-                }
-                else
-                {
-                    //Füge neuen Term beider Listen hinzu
-                    ListTerms[nDigit].Add(null);
-                }
             }
         }
-
-        public static bool CheckIfResultAlreadyExist(long NewTermResult)
+        public static void CreateTerms(int IndexA1, int IndexA2, int IndexB1, int IndexB2)
         {
+            Term Term1 = ListTerms[IndexA1][IndexA2];
+            Term Term2 = ListTerms[IndexB1][IndexB2];
+
+            //Schaue ob Term2 größer ist als Term1, wenn ja tausche diese
+            if(Term1.GetResult() < Term2.GetResult())
+            {
+                Term1 = ListTerms[IndexB1][IndexB2];
+                Term2 = ListTerms[IndexA1][IndexA2];
+            }
+
+            Term NewTerm;
+            int nDigit = IndexA1 + IndexB1;
+
+            //Addition
+            NewTerm = new AddOperator(Term1, Term2);
+            if (CheckTerm(NewTerm))
+            {
+                AddTermToLists(NewTerm,nDigit);
+            }
+
+            //Subtraction
+
+            //Multiplication
+
+            //Division
+
+            //Modulo
+
+            //Power
+
+            //Factorial
+
+        }
+        public static bool CheckTerm(Term NewTerm)
+        {
+            long TermResult = NewTerm.GetResult();
             foreach(TResult OldTermResults in ListResults)
             {
-                if(OldTermResults.Result == NewTermResult)
+                if(OldTermResults.Result == TermResult)
                 {
-                    return true;
+                    return false;
                 }
             }
-            return false;
+            if(TermResult == GoalNumber)
+            {
+                //Beende
+
+                return false;
+            }
+            return true;
+        }
+        private static void AddTermToLists(Term NewTerm, int nDigit)
+        {
+            //Füge neuen Term beider Listen hinzu
+            ListTerms[nDigit].Add(NewTerm);
+            int Index = ListTerms[nDigit].Count - 1;
+            ListResults.Add(new TResult(NewTerm.GetResult(), nDigit, Index));
         }
     }
 }
